629db392bbfecdd6af4d71e0454271232fddac88
anthonidev <softwaretoni21@gmail.com>
2022-08-31 00:53:22 -0500

    fix

--- core/settings.py
+++ core/settings.py
@@ -225,12 +225,12 @@ AUTH_USER_MODEL = "user.UserAccount"
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-# if not DEBUG:
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# DEFAULT_FROM_EMAIL = 'Belnmont - Empresa  <anthoni.api.django@gmail.com>'
-# EMAIL_HOST = os.environ.get('EMAIL_HOST')
-# EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-# EMAIL_PORT = os.environ.get('EMAIL_PORT')
-# EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
-# RECIPIENT_ADDRESS = os.environ.get('RECIPIENT_ADDRESS')
+if not DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'Belnmont - Empresa  <anthoni.api.django@gmail.com>'
+    EMAIL_HOST = os.environ.get('EMAIL_HOST')
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+    EMAIL_PORT = os.environ.get('EMAIL_PORT')
+    EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
+    RECIPIENT_ADDRESS = os.environ.get('RECIPIENT_ADDRESS')
